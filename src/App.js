9e96e5ad0768bd0c63d04ef8b3427655841d4d49
2h311 <kcodepal@outlook.com>
2022-11-06 04:41:32 -0600

    hello world. just got our landing page working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import SearchLogo from "./search.svg";
 import "./App.css";
 
-// const API_KEY = "1daeaeff";
 let API_KEY;
 if (process.env.NODE_ENV === "development") {
   API_KEY = process.env.REACT_APP_OMDB_API_KEY;
@@ -13,12 +12,32 @@ if (process.env.NODE_ENV === "development") {
 const API_URL = `http://www.omdbapi.com?apikey=${API_KEY}`;
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("Batman");
   const [movies, setMovies] = useState("");
 
+  useEffect(() => {
+    searchMovies(searchTerm);
+  }, [searchTerm]);
+
+  async function searchMovies(title) {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <img src={SearchLogo} className="App-logo" alt="logo" />
+    <div className="app">
+      <h1>Movie Land</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          placeholder="Search for movies"
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <img src={SearchLogo} className="App-logo" alt="logo" />
+      </div>
     </div>
   );
 }
